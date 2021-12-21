3a5c9c1096c6e7a5f7ee78b07923cdd4a1e5ffdb
rameshsolasa <rameshsolasa17@gmail.com>
2021-12-22 02:46:09 +0530

    Initial Commit

---
+++ build/precache-manifest.e6aa5517869585ae3d89ea380d035c56.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef05a8dae4601f658250061cbc907865",
+    "url": "/Google-Keep/index.html"
+  },
+  {
+    "revision": "45bfcaebd042ec0a142f",
+    "url": "/Google-Keep/static/js/2.783a4611.chunk.js"
+  },
+  {
+    "revision": "f272fbb9c4ebbe2958d7",
+    "url": "/Google-Keep/static/js/main.29d2da0d.chunk.js"
+  },
+  {
+    "revision": "093d8cad7bb9ef82f281",
+    "url": "/Google-Keep/static/js/runtime-main.184e6946.js"
+  }
+]);
